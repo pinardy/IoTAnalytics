5d9752c5aaf0478293fcd41ed303776dd70d3260
Pinardy <pinardy@hotmail.com>
2017-05-02 22:07:32 +0800

    Added graph display

--- DataAnalytics/MeetingRoom1.py
+++ DataAnalytics/MeetingRoom1.py
@@ -1,13 +1,24 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-import csv
 
 # Meeting room 1, sensor 1
 
+# "header = 0" means '0' index is the header
+# "parse_dates=[0]" converts the Strings under the first column into datetime64 objects
+
 plt.style.use('ggplot')
 data = pd.read_csv("datasets\\Temperature- Meeting Room 1 - Sensor 1.csv",
-                 parse_dates=[0], header=None, usecols=[0, 1])
+                 parse_dates=[0], header=0, usecols=[0, 1])
 
-# print type(data) # pandas.core.frame.DataFrame
-print data
+
+# -=-=-=-=-= PRINTING OF DATA -=-=-=-=-=
+# print data
+# print data['Date & Time']
+# print data['Value']
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+plt.plot(data['Date & Time'], data['Value'], '*')
+plt.xticks(rotation='vertical')
+
+# From python shell, type:
+# execfile("MeetingRoom1.py")
